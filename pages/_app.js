331cf71b6ccec586eec292b90e810f895662b98e
Julian Moses <julian.moses@hey.com>
2022-05-04 20:29:50 +0200

    Changed some stuff

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,14 @@ function MyApp({ Component, pageProps }) {
 
     <>
       {/* <!-- Global site tag (gtag.js) - Google Analytics --> */}
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-QQSKJ4XVVX"></Script>
+      <Script src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}></Script>
       <Script id="google-analytics" strategy='afterInteractive'>
         {`
           window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments)}
           gtag('js', new Date());
   
-          gtag('config', 'G-QQSKJ4XVVX');
+          gtag('config', ${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS});
         `}
         
       </Script>
